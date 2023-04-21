8093d1803a8c61dd67459dc471daf3aacffb8734
arun <arunkannan962@gmail.com>
2023-04-21 10:43:56 +0530

    unique email added,navbar modified,home screen modified

--- website/app/form.py
+++ website/app/form.py
@@ -1,11 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.core.exceptions import ValidationError
 class CustomUserForm(UserCreationForm):
-    username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'enter user name'}))
-    email=forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'enter E-mail'}))
-    password1=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'enter password'}))
-    password2=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Re-enter password'}))
+    username=forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'enter user name'}))
+    email=forms.EmailField(required=True,widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'enter E-mail'}))
+    password1=forms.CharField(required=True,widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'enter password'}))
+    password2=forms.CharField(required=True,widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Re-enter password'}))
     class Meta:
         model=User
         fields=['username','email','password1','password2']
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("An user with this email already exists!")
+        return email 
